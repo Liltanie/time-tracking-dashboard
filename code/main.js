8cb9c41b859a4be5d5f775a2bdc832ce73df5df1
Candela Bramucci <candelabramucci05@gmail.com>
2021-10-16 17:17:13 -0300

    code for stats in cards

---
+++ code/main.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const cards = [...document.querySelectorAll('.card')]
+const timeFrames = document.querySelector('.profile__selection')
+//usar template
+
+const showStats = (data, timeFrame) => {
+	let context
+	if (timeFrame == 'monthly') context = 'Month'
+	if (timeFrame == 'weekly') context = 'Week'
+	if (timeFrame == 'daily') context = 'Day'
+
+	for (let card in cards) {
+		const cardCurrentTime = cards[card].querySelector('.card__current-time')
+		const cardPreviousTime = cards[card].querySelector('.card__previous-time')
+		cardCurrentTime.textContent = data[card].timeframes[timeFrame].current + 'hrs'
+		cardPreviousTime.textContent = `Last ${context} - ${data[card].timeframes[timeFrame].previous}hrs`
+	}
+}
+
+fetch('https://raw.githubusercontent.com/Liltanie/time-tracking-dashboard/master/code/data.json')
+	.then(res => res.json())
+	.then(data => {
+		timeFrames.addEventListener('click', e => {
+			if (e.target.localName == 'li') {
+				;[...timeFrames.querySelectorAll('li')].forEach(e => e.classList.remove('active'))
+				e.target.classList.add('active')
+				showStats(data, e.target.classList[0])
+			}
+		})
+		showStats(data, 'weekly')
+	})
+	.catch(e => console.log(e))
